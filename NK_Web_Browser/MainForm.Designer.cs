73bf71738c1d588e0aa5f4bb7fdd321f0ce47ab8
NasirKhan <nk99technology@.gmail.com>
2021-02-06 20:15:52 +0600

    Create web browser home page,Address bar

---
+++ NK_Web_Browser/MainForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace NK_Web_Browser
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.nk_webBrowser = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.HomeToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.AddressToolStripComboBox = new System.Windows.Forms.ToolStripComboBox();
+            this.GoToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1.SuspendLayout();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(933, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.nk_webBrowser);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(933, 401);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.LeftToolStripPanelVisible = false;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 24);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.RightToolStripPanelVisible = false;
+            this.toolStripContainer1.Size = new System.Drawing.Size(933, 426);
+            this.toolStripContainer1.TabIndex = 1;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // nk_webBrowser
+            // 
+            this.nk_webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nk_webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.nk_webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.nk_webBrowser.Name = "nk_webBrowser";
+            this.nk_webBrowser.ScriptErrorsSuppressed = true;
+            this.nk_webBrowser.Size = new System.Drawing.Size(933, 401);
+            this.nk_webBrowser.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.HomeToolStripButton,
+            this.AddressToolStripComboBox,
+            this.GoToolStripButton});
+            this.toolStrip1.Location = new System.Drawing.Point(3, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(591, 25);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // HomeToolStripButton
+            // 
+            this.HomeToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.HomeToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("HomeToolStripButton.Image")));
+            this.HomeToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.HomeToolStripButton.Name = "HomeToolStripButton";
+            this.HomeToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.HomeToolStripButton.Click += new System.EventHandler(this.HomeToolStripButton_Click);
+            // 
+            // AddressToolStripComboBox
+            // 
+            this.AddressToolStripComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.AddressToolStripComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllUrl;
+            this.AddressToolStripComboBox.Name = "AddressToolStripComboBox";
+            this.AddressToolStripComboBox.Size = new System.Drawing.Size(500, 25);
+            this.AddressToolStripComboBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.AddressToolStripComboBox_KeyUp);
+            // 
+            // GoToolStripButton
+            // 
+            this.GoToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.GoToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("GoToolStripButton.Image")));
+            this.GoToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.GoToolStripButton.Name = "GoToolStripButton";
+            this.GoToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.GoToolStripButton.Click += new System.EventHandler(this.GoToolStripButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "NK Web Browser";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.WebBrowser nk_webBrowser;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton HomeToolStripButton;
+        private System.Windows.Forms.ToolStripComboBox AddressToolStripComboBox;
+        private System.Windows.Forms.ToolStripButton GoToolStripButton;
+    }
+}
+
---
+++ NK_Web_Browser/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NK_Web_Browser
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("All rights reserved Program with nk 2021", "Nk_WebBrowser", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            nk_webBrowser.Navigate("http://www.google.co.uk");
+            AddressToolStripComboBox.Text = "http://www.google.co.uk";
+        }
+
+        private void HomeToolStripButton_Click(object sender, EventArgs e)
+        {
+            nk_webBrowser.Navigate("http://www.google.co.uk");
+            AddressToolStripComboBox.Text = "http://www.google.co.uk";
+        }
+
+        private void GoToolStripButton_Click(object sender, EventArgs e)
+        {
+            nk_webBrowser.Navigate(AddressToolStripComboBox.Text);
+        }
+
+        private void AddressToolStripComboBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                nk_webBrowser.Navigate(AddressToolStripComboBox.Text);
+            }
+        }
+
+    }
+}
